5b54e1c8e6919fe9c2fe457853f34e5463c65ff2
pepars-Rashid <peparsrashid@gmail.com>
2026-05-26 07:22:24 +0300

    shadcn rtl, arabic font setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter, Noto_Sans_Arabic } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const notoSansArabic = Noto_Sans_Arabic({
+  subsets: ['arabic'],
+  variable: '--font-noto-sans-arabic',
+  display: 'swap',
+})
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="ar"
+      dir="rtl"
+      className={cn("h-full", "antialiased", notoSansArabic.variable, geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
